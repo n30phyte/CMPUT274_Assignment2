92828423314ee51635efafa6c17af170280730ff
Benjamin Kong <benjaminkong17@gmail.com>
2019-11-16 10:59:25 -0700

    Code cleanup

--- encryption.cpp
+++ encryption.cpp
@@ -1,26 +1,60 @@
 #include "encryption.h"
 
+/*
+    Description: Modular multiplication. Calculates 
+        (value1 * value2) % modulus without exceeding 32 bits at any point 
+        during computation. 
+
+    Arguments: 
+        * value1 (uint32_t): the first number to multiply.
+        * value2 (uint32_t): the second number to multiply.
+        * modulus (uint32_t): the modulus of the operation.
+    
+    Returns:
+        * answer (uint32_t): the result of modular multiplication. 
+*/
 uint32_t mulmod(uint32_t value1, uint32_t value2, uint32_t modulus)
 {
     uint32_t answer = 0;
 
+    // Take the modulus of `value2' to prevent overflow. This is justified 
+    // through modulus rules.
     value2 %= modulus;
 
     while (value1 > 0) {
+        // If the current bit is a one...
         if (value1 & 0b1) {
             answer = (answer + value2) % modulus;
         }
 
+        // Essentially divide `value1' by two.
+        value1 >>= 1;
+
+        // Essentially multiply `value2' by two, then take the modulus.
+        // We take the modulus as one step to prevent overflow.
         value2 <<= 1;
         value2 %= modulus;
-        value1 >>= 1;
+        
     }
 
+    // The last step is to take the modulus of the answer.
     answer %= modulus;
 
     return answer;
 }
 
+/*
+    Description: Fast exponentiation. Calculates (base^exponent) % modulus.
+        Procedure is described in the class notes.
+
+    Arguments: 
+        * base (uint32_t): the base of exponentiation.
+        * exponent (uint32_t): the exponent of exponentiation.
+        * modulus (uint32_t): the modulus of the operation.
+    
+    Returns:
+        * answer (uint32_t): the result of fast expoenetiation. 
+*/
 uint32_t powmod(uint32_t base, uint32_t exponent, uint32_t modulus)
 {
     uint32_t answer = 1;
@@ -30,6 +64,8 @@ uint32_t powmod(uint32_t base, uint32_t exponent, uint32_t modulus)
         if (exponent & 0b1) {
             answer = mulmod(answer, pow_x, modulus);
         }
+
+        // Essentially divide the exponent by two using bitwise operators.
         exponent >>= 1;
         pow_x = mulmod(pow_x, pow_x, modulus);
     }
