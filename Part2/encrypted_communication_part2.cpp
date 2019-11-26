1ad85e974e9459c15485f5b0f9b5f600ca7459d3
Michael Kwok <michael.kwok00@gmail.com>
2019-11-25 18:36:18 -0700

    Switch to google style cos it was getting too indented
    Removed compile_commands.json

--- Part2/encrypted_communication_part2.cpp
+++ Part2/encrypted_communication_part2.cpp
@@ -17,44 +17,42 @@ const int ID_PIN = 13;
  * Store the read bytes in buffer, directly get uint32_t in message.
  */
 union Converter {
-    uint32_t message = 0;
-    char buffer[4];
+  uint32_t message = 0;
+  char buffer[4];
 };
 
 /**
  * Stores private keys, public keys and moduli.
  */
 struct ArduinoConstants {
-    uint32_t thisPrivateKey;
-    uint32_t thisModulus;
-    uint32_t thisPublicKey;
-    uint32_t otherPublicKey;
-    uint32_t otherModulus;
+  uint32_t thisPrivateKey;
+  uint32_t thisModulus;
+  uint32_t thisPublicKey;
+  uint32_t otherPublicKey;
+  uint32_t otherModulus;
 };
 
 /**
  * The provided uint32_from_serial3() function did not seem to work, and this
  * was our solution.
  */
-uint32_t receiveUint32()
-{
-    Converter conv;
+uint32_t receiveUint32() {
+  Converter conv;
 
-    // Read 4 bytes into the `Converter' union.
-    Serial3.readBytes(conv.buffer, 4);
-    return conv.message;
+  // Read 4 bytes into the `Converter' union.
+  Serial3.readBytes(conv.buffer, 4);
+  return conv.message;
 }
 /**
  * The provided uint32_to_serial3() function did not seem to work, and this
  * was our solution.
  */
-void sendUint32(uint32_t number)
-{
-    Converter conv;
+void sendUint32(uint32_t number) {
+  Converter conv;
 
-    conv.message = number;
-    // Write 4 bytes from the "buffer".
-    Serial3.write(conv.buffer, 4);
+  conv.message = number;
+  // Write 4 bytes from the "buffer".
+  Serial3.write(conv.buffer, 4);
 }
 
 /**
@@ -69,11 +67,11 @@ void sendUint32(uint32_t number)
  * @param &properties: A reference to the "constants" of this Arduino
  * @return The decrypted character.
  */
-char receive(const ArduinoConstants& constants)
-{
-    char decrypted = powmod(receiveUint32(), constants.thisPrivateKey, constants.thisModulus);
+char receive(const ArduinoConstants& constants) {
+  char decrypted =
+      powmod(receiveUint32(), constants.thisPrivateKey, constants.thisModulus);
 
-    return decrypted;
+  return decrypted;
 };
 
 /**
@@ -82,111 +80,106 @@ char receive(const ArduinoConstants& constants)
  * @param message: the message to be encrypted and sent to the other Arduino
  * @param &properties: A reference to the "constants" of this Arduino.
  */
-void send(char message, const ArduinoConstants& constants)
-{
-    sendUint32(powmod(message, constants.otherPublicKey, constants.otherModulus));
+void send(char message, const ArduinoConstants& constants) {
+  sendUint32(powmod(message, constants.otherPublicKey, constants.otherModulus));
 }
 
 /**
  * Client message processing loop.
  *
- * Wait for input from the user. If there is a message, the Arduino will send the
- * message to the other Arduino once it has been encrypted in the 'send' function.
+ * Wait for input from the user. If there is a message, the Arduino will send
+ * the message to the other Arduino once it has been encrypted in the 'send'
+ * function.
  *
  * @param &properties: A reference to the "constants" of this Arduino.
  */
-void clientLoop(const ArduinoConstants& constants)
-{
-    if (Serial.available() > 0) {
-        char input = Serial.read();
-        Serial.flush();
+void clientLoop(const ArduinoConstants& constants) {
+  if (Serial.available() > 0) {
+    char input = Serial.read();
+    Serial.flush();
 
-        if (input == '\r') {
-            send('\r', constants);
-            send('\n', constants);
-            Serial.println();
-        } else {
-            Serial.print(input);
-            send(input, constants);
-        }
-
-        Serial3.flush();
+    if (input == '\r') {
+      send('\r', constants);
+      send('\n', constants);
+      Serial.println();
+    } else {
+      Serial.print(input);
+      send(input, constants);
     }
+
+    Serial3.flush();
+  }
 }
 
 /**
  * Server message processing loop.
  *
- * Wait for a message from the other Arduino. As soon as a new message is available,
- * it will print the message after it is decrypted in the 'receive' function.
+ * Wait for a message from the other Arduino. As soon as a new message is
+ * available, it will print the message after it is decrypted in the 'receive'
+ * function.
  *
  * @param &properties: A reference to the "constants" of this Arduino.
  */
-void serverLoop(const ArduinoConstants& constants)
-{
-    if (Serial3.available() > 0) {
-        Serial.print(receive(constants));
-    }
+void serverLoop(const ArduinoConstants& constants) {
+  if (Serial3.available() > 0) {
+    Serial.print(receive(constants));
+  }
 }
 
-uint16_t generateNumber(int minSize)
-{
-    uint16_t number = 0;
-    for (int i = 0; i < minSize; i++) {
-        auto LSB = analogRead(1) & 0b1;
-        number |= (LSB << i);
-        delay(1000);
-    }
+uint16_t generateNumber(int minSize) {
+  uint16_t number = 0;
+  for (int i = 0; i < minSize; i++) {
+    auto LSB = analogRead(1) & 0b1;
+    number |= (LSB << i);
+    delay(1000);
+  }
 }
 
-uint16_t generatePrime(int minSize)
-{
-    auto number = generateNumber(minSize);
+uint16_t generatePrime(int minSize) {
+  auto number = generateNumber(minSize);
 
-    // 2^{k+1} -1
-    const uint16_t overflowLimit = (1 << (minSize + 1)) - 1;
+  // 2^{k+1} -1
+  const uint16_t overflowLimit = (1 << (minSize + 1)) - 1;
 
-    while (!isPrime(number)) {
-        if (number == overflowLimit) {
-            number = (1 << minSize);
-        } else {
-            number++;
-        }
+  while (!isPrime(number)) {
+    if (number == overflowLimit) {
+      number = (1 << minSize);
+    } else {
+      number++;
     }
+  }
 }
 
-uint16_t generateCoprime(int minSize, uint32_t totient)
-{
-    auto number = generateNumber(minSize);
+uint16_t generateCoprime(int minSize, uint32_t totient) {
+  auto number = generateNumber(minSize);
 
-    // 2^{k+1} -1
-    const uint16_t overflowLimit = (1 << (minSize + 1)) - 1;
+  // 2^{k+1} -1
+  const uint16_t overflowLimit = (1 << (minSize + 1)) - 1;
 
-    while (gcd(number, totient) != 1) {
-        if (number == overflowLimit) {
-            number = (1 << minSize);
-        } else {
-            number++;
-        }
+  while (gcd(number, totient) != 1) {
+    if (number == overflowLimit) {
+      number = (1 << minSize);
+    } else {
+      number++;
     }
+  }
 
-    return number;
+  return number;
 }
 
 /** Waits for a certain number of bytes on Serial3 or timeout
-* @param nbytes : the number of bytes we want
-* @param timeout : timeout period (ms); specifying a negative number
-* turns off timeouts (the function waits indefinitely
-* if timeouts are turned off).
-* @return True if the required number of bytes have arrived .
-*/
-bool wait_on_serial3(uint8_t nbytes, long timeout)
-{
-    unsigned long deadline = millis() + timeout; // wraparound not a problem
-    while (Serial3.available() < nbytes && (timeout < 0 || millis() < deadline)) {
-        delay(1); // be nice , no busy loop
-    }
-    return Serial3.available() >= nbytes;
+ * @param nbytes : the number of bytes we want
+ * @param timeout : timeout period (ms); specifying a negative number
+ * turns off timeouts (the function waits indefinitely
+ * if timeouts are turned off).
+ * @return True if the required number of bytes have arrived .
+ */
+bool wait_on_serial3(uint8_t nbytes, long timeout) {
+  unsigned long deadline = millis() + timeout;  // wraparound not a problem
+  while (Serial3.available() < nbytes && (timeout < 0 || millis() < deadline)) {
+    delay(1);  // be nice , no busy loop
+  }
+  return Serial3.available() >= nbytes;
 }
 
 /**
@@ -197,141 +190,133 @@ bool wait_on_serial3(uint8_t nbytes, long timeout)
  * Calls Arduino initialization code (init())
  * Sets up pins to be on input mode for the server determination pin.
  */
-void setup()
-{
-    init();
-    Serial.begin(9600);
-    Serial3.begin(9600);
+void setup() {
+  init();
+  Serial.begin(9600);
+  Serial3.begin(9600);
 
-    pinMode(ID_PIN, INPUT_PULLUP);
+  pinMode(ID_PIN, INPUT_PULLUP);
 }
 
 /**
  * Main function of program.
  */
-int main()
-{
-    setup();
+int main() {
+  setup();
 
-    enum States {
-        Start,
-        WaitForKey,
-        WaitForAck,
-        DataExchange
-    };
+  enum States { Start, WaitForKey, WaitForAck, DataExchange };
 
-    bool serverPin = digitalRead(ID_PIN);
+  bool serverPin = digitalRead(ID_PIN);
 
-    ArduinoConstants consts;
-    States currentState = Start;
+  ArduinoConstants consts;
+  States currentState = Start;
 
-    auto p = generatePrime(14);
-    auto q = generatePrime(15);
+  auto p = generatePrime(14);
+  auto q = generatePrime(15);
 
-    uint32_t totient = (p - 1) * (q - 1);
+  uint32_t totient = (p - 1) * (q - 1);
 
-    consts.thisModulus = p * q;
+  consts.thisModulus = p * q;
 
-    if (serverPin) {
-        Serial.println("This is server.");
+  if (serverPin) {
+    Serial.println("This is server.");
 
-        bool sentKey = false;
+    bool sentKey = false;
 
-        while (true) {
-            switch (currentState) {
-            case Start:
-                if (wait_on_serial3(1, 1000)) {
-                    if (Serial3.read() == 'C') {
-                        // Request
-                        currentState = WaitForKey;
-                    }
-                }
-
-                break;
-            case WaitForKey:
-                if (wait_on_serial3(8, 1000)) {
-
-                    consts.otherPublicKey = receiveUint32();
-                    consts.otherModulus = receiveUint32();
-                    if (!sentKey) {
-                        Serial3.write("A");
-                        sendUint32(consts.thisPublicKey);
-                        sendUint32(consts.thisModulus);
-                        currentState = WaitForAck;
-                        sentKey = true;
-                    }
-                } else {
-                    currentState = Start;
-                }
-                break;
-            case WaitForAck:
-                if (wait_on_serial3(1, 1000)) {
-                    char response = Serial3.read();
-                    if (response == 'A') {
-                        currentState = DataExchange;
-                    } else if (response == 'C') {
-                        currentState = WaitForKey;
-                    }
-                } else {
-                    currentState = Start;
-                }
-                break;
-            case DataExchange:
-                serverLoop(consts);
-                break;
-            default:
-                currentState = Start;
-                break;
+    while (true) {
+      switch (currentState) {
+        case Start:
+          if (wait_on_serial3(1, 1000)) {
+            if (Serial3.read() == 'C') {
+              // Request
+              currentState = WaitForKey;
             }
-        }
-    } else {
-        Serial.println("This is client.");
+          }
 
-        // Variable reused for different time operations.
-        unsigned long time;
-
-        while (true) {
-            switch (currentState) {
-            case Start:
-
-                Serial3.write("C");
-                sendUint32(consts.thisPublicKey);
-                sendUint32(consts.thisModulus);
-                currentState = WaitForAck;
-                time = millis();
-                break;
-
-            case WaitForAck:
-
-                bool exchangeSucceed = false;
-
-                if (wait_on_serial3(9, 1000)) {
-                    if (Serial3.read() == 'A') {
-                        // Ack
-                        consts.otherPublicKey = receiveUint32();
-                        consts.otherModulus = receiveUint32();
-                        Serial3.write("A");
-                        exchangeSucceed = true;
-                    }
-                }
-
-                if (exchangeSucceed) {
-                    currentState = DataExchange;
-                } else {
-                    // if more than 1000 ms has passed and exchange has not succeeded
-                    currentState = Start;
-                }
-
-                break;
-            case DataExchange:
-                clientLoop(consts);
-                break;
-            default:
-                currentState = Start;
-                break;
+          break;
+        case WaitForKey:
+          if (wait_on_serial3(8, 1000)) {
+            consts.otherPublicKey = receiveUint32();
+            consts.otherModulus = receiveUint32();
+            if (!sentKey) {
+              Serial3.write("A");
+              sendUint32(consts.thisPublicKey);
+              sendUint32(consts.thisModulus);
+              currentState = WaitForAck;
+              sentKey = true;
             }
-        }
+          } else {
+            currentState = Start;
+          }
+          break;
+        case WaitForAck:
+          if (wait_on_serial3(1, 1000)) {
+            char response = Serial3.read();
+            if (response == 'A') {
+              currentState = DataExchange;
+            } else if (response == 'C') {
+              currentState = WaitForKey;
+            }
+          } else {
+            currentState = Start;
+          }
+          break;
+        case DataExchange:
+          serverLoop(consts);
+          break;
+        default:
+          currentState = Start;
+          break;
+      }
     }
+  } else {
+    Serial.println("This is client.");
 
-    return 0;
+    // Variable reused for different time operations.
+    unsigned long time;
+
+    while (true) {
+      switch (currentState) {
+        case Start:
+
+          Serial3.write("C");
+          sendUint32(consts.thisPublicKey);
+          sendUint32(consts.thisModulus);
+          currentState = WaitForAck;
+          time = millis();
+          break;
+
+        case WaitForAck:
+
+          bool exchangeSucceed = false;
+
+          if (wait_on_serial3(9, 1000)) {
+            if (Serial3.read() == 'A') {
+              // Ack
+              consts.otherPublicKey = receiveUint32();
+              consts.otherModulus = receiveUint32();
+              Serial3.write("A");
+              exchangeSucceed = true;
+            }
+          }
+
+          if (exchangeSucceed) {
+            currentState = DataExchange;
+          } else {
+            // if more than 1000 ms has passed and exchange has not succeeded
+            currentState = Start;
+          }
+
+          break;
+        case DataExchange:
+          clientLoop(consts);
+          break;
+        default:
+          currentState = Start;
+          break;
+      }
+    }
+  }
+
+  return 0;
 }
